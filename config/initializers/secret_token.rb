3814afd11d69be800b880bdf43623a8cb39d291a
Cecilia Suderman <csmyth7182@gmail.com>
2014-06-01 20:47:54 -0600

    Hard reset
    Destroyed old application and created new, fresh application

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MonkeyWrenchSolutions::Application.config.secret_token = '5012ce4c72fa8d26afab7f6f8a239da926c290de4f72511bd713839f2063c66294bdc586a5fbd9f157d8f98197ab098f933d528e0721848a5cf790a3a7969a46'
